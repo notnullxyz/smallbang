a025658608694d485a26f0309077043ea6b12504
Marlon van der Linde <marlonvdl@infinea.net>
2018-11-02 17:30:38 +0200

    See that I can get to redis and memcached

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,15 @@
 <?php
-phpinfo();
+
+$redis = new Redis();
+$redis->connect('smallbang-redis', 6379);
+$count = $redis->dbSize();
+echo "Redis has $count keys\n";
+
+
+$mc = new Memcached();
+$mc->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
+$mc->addServer('smallbang-memcached', 11211);
+
+$mc->set('foo', 'bar');
+$value = $mc->get('foo');
+echo "Memcached Value for foo: $value";
